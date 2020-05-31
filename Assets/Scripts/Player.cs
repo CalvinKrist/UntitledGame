f677e58480ef4f1c695ef7abb2497ff5c29bd055
CalvinKrist <calvin.krist@yahoo.com>
2020-05-30 22:20:36 -0400

    Removes bird; adds player with resources

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int startingResource = 100;
+
+    private int resource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resource = startingResource;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getResources()
+    {
+        return resource;
+    }
+
+    // TODO: make threadsafe?
+    public void addResources(int inc)
+    {
+        resource += inc;
+    }
+}
