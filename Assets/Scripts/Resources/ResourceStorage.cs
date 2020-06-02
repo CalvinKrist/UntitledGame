913fdeadfa10848e7ca934b94533ad68b5724161
CalvinKrist <calvin.krist@yahoo.com>
2020-06-02 17:40:53 -0400

    Add starting money value

--- Assets/Scripts/Resources/ResourceStorage.cs
+++ Assets/Scripts/Resources/ResourceStorage.cs
@@ -9,9 +9,13 @@ public class ResourceStorage : MonoBehaviour
 
     private Dictionary<ResourceType, int> resources;
 
+    public int STARTING_MONEY = 0;
+
     void Start()
     {
         resources = new Dictionary<ResourceType, int>();
+
+        resources[ResourceType.Money] = STARTING_MONEY;
     }
 
     public void AddResources(ResourceType type, int count)
@@ -24,10 +28,7 @@ public class ResourceStorage : MonoBehaviour
 
     public int GetResourceCount(ResourceType type)
     {
-        if (resources.ContainsKey(type))
-            return resources[type];
-        else
-            return 0;
+        return resources[type];
     }
 
 }
