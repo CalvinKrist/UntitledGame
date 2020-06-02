1e4baf30a9246c3d3fc293decdd62690a070b63f
miashaker <mes2hu@virginia.edu>
2020-06-01 21:45:03 -0400

    camera moves with arrows or wasd

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float moveSpeed = 0.5f;
+    private float scrollSpeed = 10f;
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+        }
+    }
+}
