21686a5dbf693cb489604d7c291f7bda2da9d5ef
unknown <david.l.smith166@gmail.com>
2020-06-08 20:07:10 -0400

    [Camera Movement] Altered camera move to work with iso, added scrolling. Click and drag move WIP

--- Assets/Scripts/UI/Camera/CameraMove.cs
+++ Assets/Scripts/UI/Camera/CameraMove.cs
@@ -1,43 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     bool IsMouseOverGameWindow { get { return !(0 > Input.mousePosition.x || 0 > Input.mousePosition.y || Screen.width < Input.mousePosition.x || Screen.height < Input.mousePosition.y); } }
 
-    private float moveSpeed = 0.25f;
-    private float scrollSpeed = 10f;
+    public float moveSpeed = 0.25f;
 
     void Update()
     {
-
-
         if (IsMouseOverGameWindow)
         {
             if (Input.mousePosition.x > (9 * Screen.width / 10))
             {
-                transform.position += moveSpeed * new Vector3(1, 0, 0);
+                transform.position += moveSpeed * Vector3.right;
             }
 
             if (Input.mousePosition.x < (Screen.width / 10))
             {
-                transform.position += moveSpeed * new Vector3(-1, 0, 0);
+                transform.position += moveSpeed * Vector3.left;
             }
             if (Input.mousePosition.y > (9 * Screen.height / 10))
             {
-                transform.position += moveSpeed * new Vector3(0, 0, 1);
+                transform.position += moveSpeed * Vector3.up;
             }
             if (Input.mousePosition.y < (Screen.height / 10))
             {
-                transform.position += moveSpeed * new Vector3(0, 0, -1);
+                transform.position += moveSpeed * Vector3.down;
             }
         }
-
     }
 }
